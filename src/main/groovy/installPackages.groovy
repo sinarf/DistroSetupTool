d99828a5644e0a6da399ce86125732743b360bd2
Michel Blavin <michel@blavin.fr>
2012-02-05 11:08:50 +0100

    Added auto distribution upgrade.

--- src/main/groovy/installPackages.groovy
+++ src/main/groovy/installPackages.groovy
@@ -1,17 +1,24 @@
-#!groovy
+#!/usr/bin/groovy
+
+/**
+ * Install package.
+ */
+
 def scriptDir = new File(getClass().protectionDomain.codeSource.location.path).parent
 
 def apps = ""
 new File("$scriptDir/applications.txt").eachLine { if (!it.startsWith("#")) apps += "$it "}
-def cmd = "sudo apt-get install -yy $apps"
+executeInShellAndWait("apt-get update")
+executeInShellAndWait("apt-get -yy upgrade")
+def cmd = "apt-get install -yy $apps"
 executeInShellAndWait(cmd)
-executeInShellAndWait("sudo dpkg -i /home/sinarf/Dropbox/softs/linux/*.deb")
+executeInShellAndWait("dpkg -i /home/sinarf/Dropbox/softs/linux/*.deb")
 
 sleep 5000
 
 
 def executeInShellAndWait(cmd) {
 	println "Excecuting command on a external terminal: '$cmd'"
-	def proc = "xterm -e $cmd".execute()
+	def proc = "xterm -e sudo $cmd".execute()
 	proc.waitFor()
 }
