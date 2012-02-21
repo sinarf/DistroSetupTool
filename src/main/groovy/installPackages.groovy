bb1d01a0af86fb8135761bfdb18c69ddc57db4f9
Michel Blavin <michel@blavin.fr>
2012-02-21 18:52:28 +0100

    CLOSED - task 6: The script shouldn't open a xterm http://github.com/sinarf/DistroSetupTool/issues/issue/6

--- src/main/groovy/installPackages.groovy
+++ src/main/groovy/installPackages.groovy
@@ -4,21 +4,47 @@
  * Install package.
  */
 
-def scriptDir = new File(getClass().protectionDomain.codeSource.location.path).parent
+def applicationsFilePath = "${new File(getClass().protectionDomain.codeSource.location.path).parent}/applications.txt"
 
 def apps = ""
-new File("$scriptDir/applications.txt").eachLine { if (!it.startsWith("#")) apps += "$it "}
-executeInShellAndWait("apt-get update")
-executeInShellAndWait("apt-get -yy upgrade")
-def cmd = "apt-get install -yy $apps"
-executeInShellAndWait(cmd)
-executeInShellAndWait("dpkg -i /home/sinarf/Dropbox/softs/linux/*.deb")
+executeInShellAndWait "apt-get update"
+executeInShellAndWait "apt-get -yy upgrade"
+
+println "Installing my applications from "
+
+def applicationsFile = new File(applicationsFilePath)
+if (applicationsFile.exists())
+	applicationsFile.eachLine { if (!it.startsWith("#")) apps += "$it "}
+else {
+	System.err.println "Please provide a application file: $applicationsFilePath"
+} 
+	
+executeInShellAndWait "apt-get install -yy $apps"
+// executeInShellAndWait "dpkg -i /home/sinarf/Dropbox/softs/linux/*.deb"
 
 sleep 5000
 
-
+/**
+ * 
+ * 
+ * 
+ * @param cmd command to run
+ * @exception throw a runtime exeption if cmd sending anything to the 
+ */
 def executeInShellAndWait(cmd) {
 	println "Excecuting command on a external terminal: '$cmd'"
-	def proc = "xterm -e sudo $cmd".execute()
+	def proc = "$cmd".execute()
 	proc.waitFor()
+	def out = proc.inputStream
+	def c = out.read()
+	def error = false
+	while (c > -1) {
+		char car = c
+		print  car
+		c = out.read()
+	}
+	String errMsg = proc.err.text
+	
+	if (errMsg ) throw new RuntimeException (errMsg)
+	
 }
