a826c29bfd7426da899a136cc777b610ede39f58
Michel Blavin <michel@blavin.fr>
2012-02-22 08:20:30 +0100

    code clean up.

--- src/main/groovy/installPackages.groovy
+++ src/main/groovy/installPackages.groovy
@@ -1,37 +1,40 @@
-#!/usr/bin/groovy
+#!/usr/bin/env groovy
 
 /**
- * Install package.
+ * 
+ * Install packages.
+ * 
+ * @author Michel Blavin
  */
 
 def applicationsFilePath = "${new File(getClass().protectionDomain.codeSource.location.path).parent}/applications.txt"
 
 def apps = ""
-executeInShellAndWait "apt-get update"
-executeInShellAndWait "apt-get -yy upgrade"
-
-println "Installing my applications from "
-
 def applicationsFile = new File(applicationsFilePath)
 if (applicationsFile.exists())
 	applicationsFile.eachLine { if (!it.startsWith("#")) apps += "$it "}
 else {
 	System.err.println "Please provide a application file: $applicationsFilePath"
-} 
-	
-executeInShellAndWait "apt-get install -yy $apps"
-// executeInShellAndWait "dpkg -i /home/sinarf/Dropbox/softs/linux/*.deb"
+}
+
+[
+	"Update sources..."					:	"apt-get update",
+	"Update software..."				:	"apt-get -yy upgrade",
+	"Install applications from $apps"	:	"apt-get install -yy $apps"
+].each { message, command ->
+	println message
+	executeInShell command
+}
 
 sleep 5000
 
 /**
- * 
- * 
+ * Execute anything that can run in a shell.
  * 
  * @param cmd command to run
- * @exception throw a runtime exeption if cmd sending anything to the 
+ * @exception RuntimeException throw a runtime exeption if cmd sending anything to the error stream.
  */
-def executeInShellAndWait(cmd) {
+def executeInShell(cmd) {
 	println "Excecuting command on a external terminal: '$cmd'"
 	def proc = "$cmd".execute()
 	proc.waitFor()
@@ -43,8 +46,7 @@ def executeInShellAndWait(cmd) {
 		print  car
 		c = out.read()
 	}
+	// checks for errors. 
 	String errMsg = proc.err.text
-	
 	if (errMsg ) throw new RuntimeException (errMsg)
-	
 }
