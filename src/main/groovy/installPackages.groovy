f8df8e788a2c8c93992b2b9ff715a6dc6343a43e
Michel Blavin <michel@blavin.fr>
2012-02-22 09:27:24 +0100

    CLOSED - task 9: the output of commands are displayed only at the end of the execution of a command. http://github.com/sinarf/DistroSetupTool/issues/issue/9

--- src/main/groovy/installPackages.groovy
+++ src/main/groovy/installPackages.groovy
@@ -21,7 +21,7 @@ else {
 	"Update sources..."					:	"apt-get update",
 	"Update software..."				:	"apt-get -yy upgrade",
 	"Install applications from $apps"	:	"apt-get install -yy $apps",
-	"Autoremove unused packages"	    :	"apt-get autoremove"
+	"Autoremove unused packages"	    :	"apt-get autoremove -yy"
 ].each { message, command ->
 	println message
 	executeInShell command
@@ -33,20 +33,19 @@ sleep 5000
  * Execute anything that can run in a shell.
  * 
  * @param cmd command to run
- * @exception RuntimeException throw a runtime exeption if cmd sending anything to the error stream.
+ * @exception RuntimeException throw a runtime exception if cmd sending anything to the error stream.
  */
 def executeInShell(cmd) {
 	println "Excecuting command: '$cmd'"
 	def proc = "$cmd".execute()
-	proc.waitFor()
-	def out = proc.inputStream
-	def c = out.read()
-	def error = false
+	def input = proc.inputStream
+	def c = input.read()
 	while (c > -1) {
 		char car = c
 		print  car
-		c = out.read()
+		c = input.read()
 	}
+	proc.waitFor()
 	// checks for errors. 
 	String errMsg = proc.err.text
 	if (errMsg ) throw new RuntimeException (errMsg)
